25604ad33a2c94949d8f237c28fd4e06741ce3d0
Shaen <shane@siler.io>
2023-04-01 15:39:11 -0700

    update futures and hyper

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,23 +1,21 @@
 #![allow(clippy::trivial_regex)]
 
-use futures::Future;
-use hyper::{service::service_fn_ok, Body, Method, Request, Response, Server};
+use futures::TryFutureExt;
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Method, Request, Response, Server,
+};
 use lazy_static::lazy_static;
-use std::{net::TcpListener, thread};
+use std::{convert::Infallible, net::TcpListener, sync::Arc};
+use tokio::runtime::Runtime;
 
 lazy_static! {
     static ref RE_URL: regex::Regex = regex::Regex::new("<URL>").unwrap();
 }
 
 pub struct TestServer {
+    _rt: Runtime,
     pub dir_url: String,
-    shutdown: Option<futures::sync::oneshot::Sender<()>>,
-}
-
-impl Drop for TestServer {
-    fn drop(&mut self) {
-        self.shutdown.take().unwrap().send(()).ok();
-    }
 }
 
 fn get_directory(url: &str) -> Response<Body> {
@@ -164,52 +162,67 @@ fn post_certificate(_url: &str) -> Response<Body> {
         .unwrap()
 }
 
-fn route_request(req: Request<Body>, url: &str) -> Response<Body> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/directory") => get_directory(url),
+fn route_request(req: Request<Body>, uri: &str) -> Response<Body> {
+    let method = req.method();
+    let path = req.uri().path();
+
+    match (method, path) {
+        (&Method::GET, "/directory") => get_directory(uri),
         (&Method::HEAD, "/acme/new-nonce") => head_new_nonce(),
-        (&Method::POST, "/acme/new-acct") => post_new_acct(url),
-        (&Method::POST, "/acme/new-order") => post_new_order(url),
-        (&Method::POST, "/acme/order/YTqpYUthlVfwBncUufE8") => post_get_order(url),
-        (&Method::POST, "/acme/authz/YTqpYUthlVfwBncUufE8IRWLMSRqcSs") => post_authz(url),
-        (&Method::POST, "/acme/finalize/7738992/18234324") => post_finalize(url),
-        (&Method::POST, "/acme/cert/fae41c070f967713109028") => post_certificate(url),
+        (&Method::POST, "/acme/new-acct") => post_new_acct(uri),
+        (&Method::POST, "/acme/new-order") => post_new_order(uri),
+        (&Method::POST, "/acme/order/YTqpYUthlVfwBncUufE8") => post_get_order(uri),
+        (&Method::POST, "/acme/authz/YTqpYUthlVfwBncUufE8IRWLMSRqcSs") => post_authz(uri),
+        (&Method::POST, "/acme/finalize/7738992/18234324") => post_finalize(uri),
+        (&Method::POST, "/acme/cert/fae41c070f967713109028") => post_certificate(uri),
         (_, _) => Response::builder().status(404).body(Body::empty()).unwrap(),
     }
 }
 
 pub fn with_directory_server() -> TestServer {
+    println!("creating server");
     let tcp = TcpListener::bind("127.0.0.1:0").unwrap();
     let port = tcp.local_addr().unwrap().port();
 
     let url = format!("http://127.0.0.1:{}", port);
-    let dir_url = format!("{}/directory", url);
+    let arc_url = Arc::new(url);
+    let dir_url = format!("{}/directory", arc_url.as_str());
 
-    let make_service = move || {
-        let url2 = url.clone();
-        service_fn_ok(move |req| route_request(req, &url2))
-    };
-    let server = Server::from_tcp(tcp).unwrap().serve(make_service);
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.spawn(async move {
+        println!("entered spawned fn");
+        let make_service = make_service_fn(move |_conn| {
+            let svc_url = arc_url.clone();
 
-    let (tx, rx) = futures::sync::oneshot::channel::<()>();
+            async move {
+                Ok::<_, Infallible>(service_fn(move |req| {
+                    let req_url = svc_url.clone();
+                    async move { Ok::<_, Infallible>(route_request(req, req_url.as_str())) }
+                }))
+            }
+        });
 
-    let graceful = server
-        .with_graceful_shutdown(rx)
-        .map_err(|err| eprintln!("server error: {}", err));
+        println!("preparing server");
+        let result = Server::from_tcp(tcp)
+            .unwrap()
+            .serve(make_service)
+            .map_err(|err| eprintln!("server error: {}", err));
 
-    thread::spawn(move || {
-        hyper::rt::run(graceful);
+        println!("starting server");
+        result.await
     });
 
-    TestServer {
-        dir_url,
-        shutdown: Some(tx),
-    }
+    println!("returning server");
+
+    TestServer { _rt: rt, dir_url }
 }
 
 #[test]
 pub fn test_make_directory() {
     let server = with_directory_server();
+    println!("sending request");
     let res = ureq::get(&server.dir_url).call();
+    println!("received request response");
+    println!("{:?}", res);
     assert!(res.ok());
 }
